493b92a97a25e8eb3c61407dfff8ee43a5442b18
kehtolaulu <emilia270627@gmail.com>
2018-10-20 11:08:29 +0300

    add regex check of password login and email in signup

--- dev2/script.js
+++ dev2/script.js
@@ -19,17 +19,60 @@ const createInput = (name, type, renderer) => {
 const htmlForExperience = (number) => {
            return `
                     <div class="col-5"></div>
-                    <div class="col-3">
+                    <div class="col-2">
                     <input type="text" class="form-control" id="experience_${number}" placeholder="Company">
                     </div>
                     <div class="col-2">
                     <input type="text" class="form-control" id="time" placeholder="Time period">
                     </div>
-                    <div class="col-2"></div>
+                    <div class="col-3"></div>
                     
                   `;
 }
 
 const charsLeft = () => document.getElementById('count').innerHTML = "Characters left: " + (160 - document.getElementById('comment').value.length);
+
+
+const validateForm = (id, re, msg_id, msg) => {
+    let btn = document.getElementById('submit');
+    let form = document.getElementById(id);
+    let message = document.getElementById(msg_id);
+    let check = form.value.match(re);
+    if (check == null) {
+        message.innerHTML = msg;
+        btn.disabled = true;
+        return false;
+    } else {
+        message.innerHTML = '';
+        btn.disabled = false;
+        return true;
+    }
+}
+
+const repeatPassword = () => {
+    let btn = document.getElementById('submit');
+    let message = document.getElementById('repeat_password_msg');
+    if (document.getElementById('password').value != document.getElementById('repeat_password').value) {
+        message.innerHTML = 'Passwords don\'t match!';
+        btn.disabled = true;
+        return false;
+    } else {
+        message.innerHTML = '';
+        btn.disabled = false;
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
