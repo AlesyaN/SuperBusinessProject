912f76bf9273a4f1cf3bc46845daf1811a199bbf
kehtolaulu <emilia270627@gmail.com>
2018-10-21 10:25:23 +0300

    ajax comments

--- dev2/comments.js
+++ dev2/comments.js
@@ -17,32 +17,21 @@ const createComment = (textId) => {
             text: text
         },
         success: (data) => {
-            addComment(text);
+            // addComment(text);
+
+            let list = $('#comments-list');
+            list.append('<div class="comment-item">
+                            <a href="/profile/${user.id}" id="comment-username">${user.username}</a>
+                            <p id="comment-text">${text}</p>
+                            <small id="comment-date">${comment.date}</small>
+                            
+                            <br>
+                            <button class="btn btn-danger">Delete comment</button>
+                            <button class="btn btn-primary">Edit comment</button>
+                            <hr>
+                        </div>'
+            );
         }
     });
 };
 
-// const addComment = (text) => {
-//     let comment = document.createElement('div');
-//     comment.setAttribute('class', 'comment-item');
-//     let comment_username = document.createElement('a');
-//     comment.appendChild
-
-//     let container = document.getElementById('comments-list');
-//     container.appendChild(comment);
-// }
-
-//  <div class="comments-list">
-//           <div class="comment-item">
-          
-//             <a href="" id="comment-username">Username</a>
-//             <p id="comment-text">comment</p>
-//             <small id="comment-date">22.10.2018</small>
-            
-//             <br>
-//             <button class="btn btn-danger">Delete comment</button>
-//             <button class="btn btn-primary">Edit comment</button>
-//             <hr>
-
-//           </div>
-//         </div>
